0a5794a0c433247222959c3a520412681bb3e1be
seungjoonrim <rim.seungjoon@gmail.com>
2017-12-22 13:33:36 -0500

    Finish Section 5

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
+// Import a library to help create a component
+import React from 'react';
 import { AppRegistry } from 'react-native';
-import App from './App';
+import Header from './src/components/header';
 
+// Create a component
+const App = () => (
+    <Header />
+);
+
+// Render the component to the device
 AppRegistry.registerComponent('albums', () => App);
---
+++ src/components/header.js
@@ -0,0 +1,9 @@
+// Import libraries for making a component
+import React from 'react';
+import { Text } from 'react-native';
+
+// Make a compnent
+const Header = () => <Text>ALBUMS</Text>;
+
+// Make the component available to other parts of the app
+export default Header;
